2f7a6dfbccce0d34b75d02ac18eff5a97c02d92a
saymasuhe <saymasuhe911@gmail.com>
2024-06-30 17:12:10 +0600

    database file added

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
     $hostName = "localhost";
     $dbUser = "root";
     $dbPassword = "";
-    $dbName = "login__register";
+    $dbName = "blog_php";
 
     $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 
